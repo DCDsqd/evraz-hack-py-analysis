8925f9fa5ad0b4349aaec138d6c3d4c9456ea81c
storst1 <kokakolazzz100@mail.ru>
2024-11-30 02:29:13 +0300

    feat(): add ts to archive names

--- backend/src/archive_handler.py
+++ backend/src/archive_handler.py
@@ -2,6 +2,7 @@ from io import BytesIO
 import os
 import uuid
 import zipfile
+from datetime import datetime
 
 
 def parse_archive(archive_content: bytes, root_dir: str = "extracted_files"):
@@ -12,8 +13,13 @@ def parse_archive(archive_content: bytes, root_dir: str = "extracted_files"):
     :param root_dir: Корневая папка для извлечения файлов.
     :return: Путь к извлеченной папке и строку с иерархией файлов и папок.
     """
-    # Генерация уникального идентификатора для каждой сессии
-    unique_dir = str(uuid.uuid4())  # Создаем уникальную папку для каждого запроса
+
+    # Получаем текущую дату и время и форматируем их
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Создаем уникальную папку для каждого запроса, добавляя таймстамп
+    unique_dir = f"{timestamp}_{uuid.uuid4()}"
+    
     extract_path = os.path.join(root_dir, unique_dir)
 
     try:
